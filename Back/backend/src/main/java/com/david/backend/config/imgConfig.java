017f6d82ae27094a5d20b5af8a1424e474c56472
Davidjt04 <dajantor@gmail.com>
2025-05-27 18:22:02 +0200

    vistas de adminstrador terminadas, mirar metodo borrar y acceso a objetos foraneos

---
+++ Back/backend/src/main/java/com/david/backend/config/imgConfig.java
@@ -0,0 +1,15 @@
+package com.david.backend.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class imgConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //todo lo que empiece en la url por /img tendrá esta configuracion 
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:///C:/xampp/htdocs/TrabajoRecuperacion/Back/img/");
+    }
+}
